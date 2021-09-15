bee6510500223729c496f25eb3ac7d9edf98dcad
Abderrahmane <abdrr97@gmail.com>
2021-09-15 12:49:10 +0100

    fixing some issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Github from './components/github/Github'
-import RandomUser from './components/RandomUser'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Error from './pages/Error'
 
+import { ROUTES } from './routes'
+
 const App = () => {
   return (
     <main className='container'>
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path={ROUTES.HOME} component={Home} />
           <Route path='/contact' component={Contact} />
-          <Route path='/p/:username' component={Github} />
+          <Route path={ROUTES.PROFILE} component={Github} />
 
-          <Route path='*' component={Error} />
+          <Route path={ROUTES.NOT_FOUND} component={Error} />
         </Switch>
       </Router>
     </main>
